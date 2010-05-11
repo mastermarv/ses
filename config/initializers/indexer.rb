fef64bfbd727b32576d0c9e001a1f08376a1ce89
David Császár <david.csaszar@infopark.de>
2010-05-11 17:31:23 +0200

    Removed workaround for empty validUntil

--- config/initializers/indexer.rb
+++ config/initializers/indexer.rb
@@ -1,12 +1,12 @@
 Infopark::SES::Indexer.index_fields do |obj|
-  if obj.released? # && obj.suppress_export == 0
+  if obj.released? # && obj.suppress_export == 0 && !obj.image? && !obj.generic?
     {
       :id => obj.id,
       :name => obj.name,
       :path => obj.path,
       :body => obj.body,
       :valid_from => obj.valid_from.to_iso,
-      :valid_until => obj.valid_until.try(:to_iso) || ('9' * 14),
+      :valid_until => obj.valid_until.try(:to_iso),
     }
   end
 end
