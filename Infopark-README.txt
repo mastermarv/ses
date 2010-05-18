9be04b77495445082f0ec44acd0af35d4ab44298
David Császár <david.csaszar@infopark.de>
2010-05-18 15:56:48 +0200

    Minor improvements due to feedback
    * use TCL file join instead of hardcoded path
    * make Infopark README more useful

--- Infopark-README.txt
+++ Infopark-README.txt
@@ -1,5 +1,13 @@
 Requirements:
 
 * config/license.xml
-* edit config/initializers/rails_connector.rb:
+* edit config/initializers/rails_connector.rb
 * edit config/database.yml
+
+Hints:
+
+* edit config/initializers/indexer.rb to configure fields
+* use script/control to start and stop
+* rake index:all
+
+See online documentation for further information.
--- objectChangedCallback.tcl
+++ objectChangedCallback.tcl
@@ -1,6 +1,5 @@
-
 proc objectChangedCallback {messages} {
-    set f [open "|env INSTANCE=[app get instanceName] ruby [app get scriptDir]/serverCmds/publish_object_changes.rb" w]
+    set f [open "|env INSTANCE=[app get instanceName] ruby [file join [app get scriptDir] serverCmds publish_object_changes.rb]" w]
     foreach m $messages {
         puts $f $m
     }
