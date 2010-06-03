42b007c28937a866e25c5d8507ee45f74f4da93a
David Császár <david.csaszar@infopark.de>
2010-06-03 12:05:57 +0200

    Changes for daemon-spawn 0.3.0

--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,7 @@ Rails::Initializer.run do |config|
   config.gem 'rsolr'
   config.gem 'infopark_rails_connector'
   config.gem 'ruby-progressbar', :lib => 'progressbar'
+  config.gem 'daemon-spawn', :lib => 'daemon_spawn', :version => ">= 0.3.0"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
