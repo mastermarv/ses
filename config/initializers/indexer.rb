186302c15d51e03a56c9c4e6d980c750418e86eb
David Császár <david.csaszar@infopark.de>
2010-06-07 15:50:28 +0200

    Replaced commented out code with nicer version

--- config/initializers/indexer.rb
+++ config/initializers/indexer.rb
@@ -1,5 +1,5 @@
 Infopark::SES::Indexer.index_fields do |obj|
-  if obj.released? # && obj.suppress_export == 0 && !obj.image?
+  if obj.released? # && !obj.suppress_export? && !obj.image?
     plain_body = Infopark::SES::Filter::text_via_solr_cell(obj) if obj.generic?
     {
       :id => obj.id,
