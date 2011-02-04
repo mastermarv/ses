90b131ab435d11106deaaef0d7dde79f3e044705
Thomas Ritz <tritz@infopark.de>
2011-02-04 01:41:16 +0100

    rake index:all generator

--- generators/infopark_ses/infopark_ses_generator.rb
+++ generators/infopark_ses/infopark_ses_generator.rb
@@ -4,6 +4,9 @@ class InfoparkSesGenerator < Rails::Generator::Base
       m.directory 'config/initializers'
       m.file 'indexer.rb', 'config/initializers/indexer.rb'
       m.file 'filter.rb', 'config/initializers/filter.rb'
+
+      m.directory 'lib/tasks'
+      m.file 'index.rake', 'lib/tasks/index.rake'
     end
   end
 end
