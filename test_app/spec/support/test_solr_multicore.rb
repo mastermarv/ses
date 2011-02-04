d260216dfc3336470862b3052638657fc8b1688c
Thomas Ritz <tritz@infopark.de>
2011-02-04 03:47:00 +0100

    rsync non-verbose

--- test_app/spec/support/test_solr_multicore.rb
+++ test_app/spec/support/test_solr_multicore.rb
@@ -2,8 +2,8 @@ class TestSolrMulticore
 
   def setup
     system "../bin/ses-apache-solr install"
-    system "rsync -av ~/apache-solr/example/solr/conf/ ~/apache-solr/example/multicore/core0/conf/"
-    system "rsync -av ~/apache-solr/example/solr/conf/ ~/apache-solr/example/multicore/core1/conf/"
+    system "rsync -a ~/apache-solr/example/solr/conf/ ~/apache-solr/example/multicore/core0/conf/"
+    system "rsync -a ~/apache-solr/example/solr/conf/ ~/apache-solr/example/multicore/core1/conf/"
     system "../bin/ses-apache-solr start:multicore"
   end
 
